92a2bef4d821bb643723731797b1dd5111d84378
Hritik /kumar <113236034+hritsk@users.noreply.github.com>
2023-05-20 15:57:40 +0530

    Delete substrings.java

--- substrings.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class substrings {
-    public static void main(String args[]) {
-        String sentence ="TonyStark";
-        String name =sentence.substring(4);
-        System.out.println(name);
-        //Strings are immutable
-    }
-}
