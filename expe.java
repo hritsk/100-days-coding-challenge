2604e7614cda54aef3c04de0b04659d86afd41b7
Hritik /kumar <113236034+hritsk@users.noreply.github.com>
2023-05-20 15:56:01 +0530

    Delete expe.java

--- expe.java
+++
@@ -1,9 +0,0 @@
-public class expe {
-    public static void main(String args[]) {
-        int x = 2, y = 5;
-            int exp2 = (x * (y / x));
-            System.out.print(exp2);
-        
-    }
-    
-}
