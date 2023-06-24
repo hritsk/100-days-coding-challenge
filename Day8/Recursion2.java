1cfd6d37ef78aa7f9d420da4e394176e6f8f6373
Hritik <hritiksingh2902@gmail.com>
2023-06-24 23:45:20 +0530

    Day_8

---
+++ Day8/Recursion2.java
@@ -0,0 +1,17 @@
+//Print numbers in inceasing order.....
+import java.util.*;
+public class Recursion2 {
+    public static void printInc(int n) {
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+        
+    }
+    public static void main(String args[]) {
+        int n = 10;
+        printInc(n);
+    }
+}
