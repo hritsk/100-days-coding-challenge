e14a6a920865c7a0ca48a8cce1cd658978ff3b7f
Hritik <hritiksingh2902@gmail.com>
2023-06-25 23:54:23 +0530

    Day_9

---
+++ Day9/Recursion4.java
@@ -0,0 +1,16 @@
+//Sum of n naturak numbers....
+import java.util.*;
+public class Recursion4 {
+    public static int calcSum (int n) {
+        if(n == 1){
+            return 1;
+        }
+        int snm1 = calcSum(n-1);
+        int sn = calcSum(n-1);
+        return sn;
+    }
+    public static void main(String args[]) {
+        int n =9;
+        System.out.println(calcSum(n));
+    }
+}
